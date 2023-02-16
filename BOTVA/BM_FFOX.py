35dcbf4654371ad0f0588e88458d46bbe45fc215
Stanislau Mikulich <42565957+stanislaumich@users.noreply.github.com>
2023-02-16 14:04:55 +0300

    AUTO FROM WORK 16.02.2023 14:04:54,33

--- BOTVA/BM_FFOX.py
+++ BOTVA/BM_FFOX.py
@@ -18,11 +18,17 @@ def main():
     element.submit()
     time.sleep(3)
     driver.get("https://g1.botva.ru/clan_members.php?id=21148")
+    time.sleep(3)
     now = date.today()
-    s = str(now.day)+"." + str(now.month)+"." + str(now.year)
-    with open(s+"_KLAN.html", "w", encoding="utf-8") as file:
+    ds = str(now.day)+"." + str(now.month)+"." + str(now.year)
+    with open(ds+"_KLAN.html", "w", encoding="utf-8") as file:
         file.write(driver.page_source)
-
+    #elements = driver.find_elements(By.CLASS_NAME, "profile ")
+    #for el in elements:
+    #    print(el.text)
+    all_cookies = driver.get_cookies()
+    for cookie_name, cookie_value in all_cookies:
+        print("%s : %s", cookie_name, cookie_value)
 
 if __name__ == "__main__":
     main()
