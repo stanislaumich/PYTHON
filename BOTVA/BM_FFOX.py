8f30923c9edd71b2adeb2e670632aad23f06a810
Stanislau Mikulich <42565957+stanislaumich@users.noreply.github.com>
2023-02-16 15:33:05 +0300

    AUTO FROM WORK 16.02.2023 15:33:04,67

--- BOTVA/BM_FFOX.py
+++ BOTVA/BM_FFOX.py
@@ -24,11 +24,27 @@ def main():
     with open(ds+"_KLAN.html", "w", encoding="utf-8") as file:
         file.write(driver.page_source)
     #elements = driver.find_elements(By.CLASS_NAME, "profile ")
-    #for el in elements:
-    #    print(el.text)
-    all_cookies = driver.get_cookies()
-    for cookie_name, cookie_value in all_cookies:
-        print("%s : %s", cookie_name, cookie_value)
+    elements = driver.find_elements("xpath", '//tr[contains(@class, "row_")]')
+    for el in elements:
+        nik = el.find_element(By.CLASS_NAME, "profile ").text
+        print(nik)
+        bm = el.find_element(By.CLASS_NAME, "right").text
+        print(bm)
+        sl = el.find_element(By.CLASS_NAME, "nowrapi").text
+        print(sl)
+        #ts = el.text
+        #ar = ts.split(" ")
+        #ar[2] = ar[2].replace('.', '')
+        #ar[4]=ar[4].replace('.','',-1)
+        #print(ar)
+        #e = ar[2]
+        #e = e.replace('.', '', -1)
+        #print(e)
+        ##print(ts)
+
+    #all_cookies = driver.get_cookies()
+    #for cookie_name, cookie_value in all_cookies:
+    #    print("%s : %s", cookie_name, cookie_value)
 
 if __name__ == "__main__":
     main()
