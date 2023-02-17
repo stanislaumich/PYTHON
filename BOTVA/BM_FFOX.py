ae700bd9b329ced9a96e812fc1bcc1795b097214
Stanislau Mikulich <42565957+stanislaumich@users.noreply.github.com>
2023-02-17 16:37:27 +0300

    AUTO FROM WORK 17.02.2023 16:37:27,53

--- BOTVA/BM_FFOX.py
+++ BOTVA/BM_FFOX.py
@@ -24,7 +24,7 @@ def main():
     id  INTEGER, val VARCHAR2 (1000))""")
     nw = datetime.now()
     dt = nw.strftime("%d.%m.%Y")
-    tm = nw.strftime("%I:%M")
+    tm = nw.strftime("%I-%M")
 
     driver = webdriver.Chrome()
 
@@ -46,7 +46,6 @@ def main():
     #ds = str(now.day)+"." + str(now.month)+"." + str(now.year)
     with open(dt+"_"+tm+"_KLAN.html", "w", encoding="utf-8") as file:
         file.write(driver.page_source)
-    #elements = driver.find_elements(By.CLASS_NAME, "profile ")
 
     fl = []
     elements = driver.find_elements("xpath", '//tr[contains(@class, "row_")]')
@@ -61,81 +60,71 @@ def main():
         print(zv)
         bob = (nik, bm, sl, dt, tm, zv)
         fl.append(bob)
-        #cursor.execute("INSERT INTO KLAN (nik, bm, sl, dt, tm, dop) VALUES (?, ?, ?, ?, ?, ?)", bob)
-        #people = [("Sam", 28), ("Alice", 33), ("Kate", 25)]
-        #cursor.executemany("INSERT INTO people (name, age) VALUES (?, ?)", people)
     cursor.executemany("INSERT INTO KLAN (nik, bm, sl, dt, tm, dop) VALUES (?, ?, ?, ?, ?, ?)", fl)
     con.commit()
 
-    '''   
     # пробуем работать со списком походов в подзем
     f1 = open('podzem.txt')
     flist = f1.readlines()
     for el in flist:
         crt = el.split('\t')
-        print(crt[0])# 1 2
-        #driver.get(el[1])
-    '''
-    dt = "01.01.2023"#crt[0]
-    href = "https://g1.botva.ru/monster.php?a=monsterpve&do_cmd=log&raid=903197&id=2648065&key=de67129a78b6b4fe08e4e69fbfa09eb3"
-    idp0=href.split("&")
-    idp1 = idp0[3].split("=")
-    idp = idp1[1]
+        print(crt[0])
+        # вот это для каждой строки
+        dt = crt[0]
+        for i in range(1, 3):
+            # первая и вторая ссылки в строке
+            href = crt[i]
+            #href = "https://g1.botva.ru/monster.php?a=monsterpve&do_cmd=log&raid=903197&id=2648065&key=de67129a78b6b4fe08e4e69fbfa09eb3"
+            idp0 = href.split("&")
+            idp1 = idp0[3].split("=")
+            idp = idp1[1]
+            driver.get(href)
+            sleep(10)
+            master = driver.find_element(By.CLASS_NAME, "profile ").text
+            print(f"Master: {master}")
+            sql_select_query = """insert into podzem (dt, num, nik, id, val) values(?,?,?,?,0)"""
+            bob = (dt, i, master, -1)
+            cursor.execute(sql_select_query, bob)
+            con.commit()
+            elements = driver.find_elements(By.CLASS_NAME, "round3")
+            cnt = 0
+            bl = []
+            for elz in elements:
+                cnt = cnt + 1
+                ts = elz.text
+                ts = ts.replace("...", "%")
+                sql_select_query = """select nik from klan where nik like ?"""
+                cursor.execute(sql_select_query, (ts,))
+                record = cursor.fetchone()
+                ts = record[0]
+                print(ts)
+                bob = (dt, i, ts, cnt)
+                bl.append(bob)
+            sql_select_query = """insert into podzem (dt, num, nik, id, val) values(?,?,?,?,0)"""
+            cursor.executemany(sql_select_query, bl)
+            con.commit()
+            on = (dt, i, "", -2, href)
+            sql_select_query = """insert into podzem (dt, num, nik, id, val) values(?,?,?,?,?)"""
+            cursor.execute(sql_select_query, on)
+            con.commit()
+            loter = int(idp) % cnt
+            if loter == 0:
+                loter = cnt
+            pts = idp + f" mod {cnt} = " + str(loter)
+            on = (i, dt, loter)
+            sql_select_query = """select nik from podzem where num = ? and dt = ? and id = ?"""
+            cursor.execute(sql_select_query, on)
+            pobed1 = cursor.fetchone()
+            pobed = pobed1[0]
 
-    driver.get(href)
-    sleep(10)
-    # guild11
-    master = driver.find_element(By.CLASS_NAME, "profile ").text#
-    print(f"Master: {master}")
-    sql_select_query = """insert into podzem (dt, num, nik, id, val) values(?,?,?,?,0)"""
-    bob = (dt, 1, master, -1)
-    cursor.execute(sql_select_query, bob)
-    con.commit()
-    elements = driver.find_elements(By.CLASS_NAME, "round3")
-    cnt = 0
-    bl = []
-    for el in elements:
-        cnt = cnt + 1
-        ts = el.text
-        ts = ts.replace("...", "%")
-        sql_select_query = """select nik from klan where nik like ?"""
-        cursor.execute(sql_select_query, (ts,))
-        record = cursor.fetchone()
-        ts = record[0]
-        print(ts)
-        bob = (dt, 1, ts, cnt)
-        bl.append(bob)
-    sql_select_query = """insert into podzem (dt, num, nik, id, val) values(?,?,?,?,0)"""
-    cursor.executemany(sql_select_query, bl)
-    con.commit()
-    on = (dt, -2, "", href)
-    sql_select_query = """insert into podzem (dt, num, nik, id, val) values(?,?,?,?,0)"""
-    cursor.execute(sql_select_query, on)
-    con.commit()
-    loter = int(idp) % cnt
-    pts = idp + f" mod {cnt} = " + str(loter)
-    on = (dt, loter)
-    sql_select_query = """select nik from podzem where num = 1 and dt = ? and id = ?"""
-    cursor.execute(sql_select_query, on)
-    pobed = cursor.fetchone()[0]
-    #pobed = record[0]
-    on = (dt, -3, pobed, pts)
-    sql_select_query = """insert into podzem (dt, num, nik, id, val) values(?,?,?,?,0)"""
-    cursor.execute(sql_select_query, on)
-    con.commit()
-        #ts = el.text
-        #ar = ts.split(" ")
-        #ar[2] = ar[2].replace('.', '')
-        #ar[4]=ar[4].replace('.','',-1)
-        #print(ar)
-        #e = ar[2]
-        #e = e.replace('.', '', -1)
-        #print(e)
-        ##print(ts)
+            on = (dt, i, pobed, -3, pts)
+            sql_select_query = """insert into podzem (dt, num, nik, id, val) values(?,?,?,?,?)"""
+            cursor.execute(sql_select_query, on)
+            con.commit()
 
-    #all_cookies = driver.get_cookies()
-    #for cookie_name, cookie_value in all_cookies:
-    #    print("%s : %s", cookie_name, cookie_value)
+
+
+
 '''
 from threading import Thread
 
@@ -148,7 +137,9 @@ thread1 = Thread(target=exit_check)
 thread1.start() 
 
 '''
-
+# all_cookies = driver.get_cookies()
+# for cookie_name, cookie_value in all_cookies:
+#    print("%s : %s", cookie_name, cookie_value)
 
 if __name__ == "__main__":
     main()
