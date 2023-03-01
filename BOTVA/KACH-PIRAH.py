ddf58876a31393dd603818c6507ca17f4c35fe9d
stanislaumich <staslz42stas@gmail.com>
2023-03-02 00:17:05 +0300

    AUTO FROM HOME 02.03.2023  0:17:03,84

--- BOTVA/KACH-PIRAH.py
+++ BOTVA/KACH-PIRAH.py
@@ -19,9 +19,9 @@ from selenium import webdriver
 
 
 def main():
-    print("[INFO]Нужно помнить что нахождение в некоторых локациях, например подзем, не дает обработать казну")
-    #config = configparser.ConfigParser()  # создаём объекта парсера
-    #config.read("s:\\config.ini")  # читаем конфиг
+    print("[INFO]Нужно помнить что нахождение в некоторых локациях, например подзем, не дает отработать прокачку")
+    config = configparser.ConfigParser()  # создаём объекта парсера
+    config.read("s:\\config.txt")  # читаем конфиг
 
     myp = os.path.dirname(os.path.realpath(__file__)) + "\SELENIUM"
     print("Путь профиля Chrome: " + myp)
@@ -31,30 +31,31 @@ def main():
     options.add_argument("--profile-directory=BOTVA")
     driver = webdriver.Chrome(options=options)
     print("Логин...  ")
+
+    driver.get("http://botva.ru")
+    element = driver.find_element(By.CLASS_NAME, "sign_in")
+    element.click()
+    element = driver.find_element("name", "email")
+    element.send_keys(config["LOGIN"]["username"])
+    #element.send_keys("wiwali@mail.ru")
+    element = driver.find_element("name", "password")
+    element.send_keys(config["LOGIN"]["password"])
+    #element.send_keys("QYUR8NUANE")
+    element = driver.find_element(By.CLASS_NAME, "submit_by_ajax_completed")
+    element.submit()
+    sleep(3)
+    # залогинились и подождали 3 секунды ищем кнопку кача
+    element1 = driver.find_element(By.CLASS_NAME, "mr-20")
+    element1.click()
+    sleep(1)
+    element2 = driver.find_element(By.CLASS_NAME, "training_tab_2")
+    element2.click()
+    sleep(1)
     for i in range(5):
-        driver.get("http://botva.ru")
-        element = driver.find_element(By.CLASS_NAME, "sign_in")
-        element.click()
-        element = driver.find_element("name", "email")
-        #element.send_keys(config["LOGIN"]["username"])
-        element.send_keys("wiwali@mail.ru")
-        element = driver.find_element("name", "password")
-        #element.send_keys(config["LOGIN"]["password"])
-        element.send_keys("QYUR8NUANE")
-        element = driver.find_element(By.CLASS_NAME, "submit_by_ajax_completed")
-        element.submit()
-        sleep(3)
-        # залогинились и подождали 3 секунды ищем кнопку кача
-        element = driver.find_element(By.CLASS_NAME, "mr-20")
-        element.click()
-        sleep(1)
-        element = driver.find_element(By.CLASS_NAME, "training_tab_2")
-        element.click()
-        sleep(1)
         # сила - power, защита - block, ловкость - dexterity, масса - endurance, мастерство - charisma
-        element = driver.find_element(By.ID, "power")
-        element.clear()
-        element.send_keys("99999")
+        element3 = driver.find_element(By.ID, "power")
+        element3.clear()
+        element3.send_keys("9")
         #element = driver.find_element(By.ID, "block")
         #element.clear()
         #element.send_keys("99999")
@@ -68,8 +69,9 @@ def main():
         # element.clear()
         #element.send_keys("99999")
         sleep(2)
-        element1 = driver.find_element(By.ID, "btn_fish")
-        element1.click()
+        element4 = driver.find_element(By.ID, "btn_fish")
+        element4.click()
+        #driver.refresh()
 
 
 if __name__ == "__main__":
