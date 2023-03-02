9a97447204631f206086533f666efd6b80985c37
Stanislau Mikulich <42565957+stanislaumich@users.noreply.github.com>
2023-03-02 12:17:26 +0300

    AUTO FROM WORK 02.03.2023 12:17:23,27

--- BOTVA/KACH-PIRAH.py
+++ BOTVA/KACH-PIRAH.py
@@ -1,10 +1,22 @@
 '''
 Программа для прокача ботвы пирахами
-в командной строке нужно выполнить
-pip install selenium
+в командной строке нужно выполнить только первый раз, больше не надо
+pip install selenium configparser
 для установки средств управления браузером
-в файле config.ini прописать логин и пароль для входа в ботву
+в файле config.txt прописать логин и пароль для входа в ботву
+и положить его рядом с этим скриптом
 
+Установленный питон, версия любая в принципе но на винде 7 и ниже могут быть сложности
+
+Скрипт лучше всего положить в отдельную папку на диске, не на рабочем столе
+
+При работе - откроется черное окно, откроется браузер,
+залогинится и начнет качать, затем закроется
+во время работы можно окна свернуть и заниматься своими делами
+
+При работе в папке скрипта создастся папка SELENIUM - это кэш браузера
+иногда бывает глюк - скрипт падает с ошибкой, можно папку удалить
+скрипт её пересоздаст
 
 
 
@@ -16,12 +28,24 @@ from datetime import datetime
 import configparser
 import sqlite3
 from selenium import webdriver
+import mysettings
+'''
+Ниже идут настройки 
+'''
+N = 50 # Сколько раз выполнять операцию прокачки
+V1 = "1" # Сила                        сколько статов качнуть за 1 раз, от 0 до 99999, у меня 1  для теста
+V2 = "1" # Защита
+V3 = "0" # Ловкость       # у меня ловкость максималка поэтому 0, если на макс поставить хоть 1 не даст качать ничего
+V4 = "1" # Масса
+V5 = "1" # Мастерство                  любая комбинация статов от 0 до 99999
 
+pause = 5 # это настройка паузы, если компьютер не успевает обсчитать статы надо увеличивать
 
+'''---------------------------------------------------------------------------'''
 def main():
     print("[INFO]Нужно помнить что нахождение в некоторых локациях, например подзем, не дает отработать прокачку")
-    config = configparser.ConfigParser()  # создаём объекта парсера
-    config.read("s:\\config.txt")  # читаем конфиг
+    #config = configparser.ConfigParser()  # создаём объекта парсера
+    #config.read("config.txt")  # читаем конфиг
 
     myp = os.path.dirname(os.path.realpath(__file__)) + "\SELENIUM"
     print("Путь профиля Chrome: " + myp)
@@ -36,11 +60,11 @@ def main():
     element = driver.find_element(By.CLASS_NAME, "sign_in")
     element.click()
     element = driver.find_element("name", "email")
-    element.send_keys(config["LOGIN"]["username"])
-    #element.send_keys("wiwali@mail.ru")
+    #element.send_keys(config["LOGIN"]["username"])
+    element.send_keys(mysettings.login)
     element = driver.find_element("name", "password")
-    element.send_keys(config["LOGIN"]["password"])
-    #element.send_keys("QYUR8NUANE")
+    #element.send_keys(config["LOGIN"]["password"])
+    element.send_keys(mysettings.passw)
     element = driver.find_element(By.CLASS_NAME, "submit_by_ajax_completed")
     element.submit()
     sleep(3)
@@ -51,27 +75,27 @@ def main():
     element2 = driver.find_element(By.CLASS_NAME, "training_tab_2")
     element2.click()
     sleep(1)
-    for i in range(5):
+    for i in range(N):
         # сила - power, защита - block, ловкость - dexterity, масса - endurance, мастерство - charisma
         element3 = driver.find_element(By.XPATH, "//input[@id='power']")
         element3.clear()
-        element3.send_keys("1")
-        #element = driver.find_element(By.ID, "block")
-        #element.clear()
-        #element.send_keys("99999")
-        #element = driver.find_element(By.ID, "dexterity")
-        # element.clear()
-        #element.send_keys("99999")
-        #element = driver.find_element(By.ID, "endurance")
-        # element.clear()
-        #element.send_keys("99999")
-        #element = driver.find_element(By.ID, "charisma")
-        # element.clear()
-        #element.send_keys("99999")
-        sleep(2)
+        element3.send_keys(V1)
+        element3 = driver.find_element(By.XPATH, "//input[@id='block']")
+        element3.clear()
+        element3.send_keys(V2)
+        element3 = driver.find_element(By.XPATH, "//input[@id='dexterity']")
+        element3.clear()
+        element3.send_keys(V3)
+        element3 = driver.find_element(By.XPATH, "//input[@id='endurance']")
+        element3.clear()
+        element3.send_keys(V4)
+        element3 = driver.find_element(By.XPATH, "//input[@id='charisma']")
+        element3.clear()
+        element3.send_keys(V5)
+        sleep(pause)
         element4 = driver.find_element(By.XPATH, "//input[@id='btn_fish']")
         element4.click()
-        #driver.refresh()
+        sleep(2)
 
 
 if __name__ == "__main__":
